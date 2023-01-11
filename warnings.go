99e6ae7cf9daecd0ec0cf5593534132111dd3a4a
Konrad Wojas <github@m.wojas.nl>
2023-01-11 14:58:06 +0800

    warnings: wrap errors
    Make warning wrap an error instead of message string. This allows
    unwrapping any original errors and makes it possible to directly use
    Warning to wrap an existing error.
    
    This also enables the use of %w in Warnf.

--- warnings.go
+++ warnings.go
@@ -9,21 +9,25 @@ import (
 // It will not cause the health check to fail, but the warning will appear
 // in the JSON.
 type Warning struct {
-	msg string
+	Err error
 }
 
 func (w Warning) Error() string {
-	return w.msg
+	return w.Err.Error()
+}
+
+func (w Warning) Unwrap() error {
+	return w.Err
 }
 
 // Warn returns a Warning with given message
 func Warn(msg string) error {
-	return Warning{msg: msg}
+	return Warning{errors.New(msg)}
 }
 
 // Warnf formats a Warning
 func Warnf(format string, args ...interface{}) error {
-	return Warning{msg: fmt.Sprintf(format, args...)}
+	return Warning{fmt.Errorf(format, args...)}
 }
 
 // IsWarning returns true if the error is a Warning instead of a failure.
